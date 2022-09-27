8dd61ffd7f099592c36484735217030ed3e7fba9
TaislaineMassucato <104451227+TaislaineMassucato@users.noreply.github.com>
2022-09-27 09:55:37 -0300

    Finalização Projeto (Périodos do Dia)

--- exercícios/dianoitetarde.js
+++
@@ -1,7 +0,0 @@
-function carregar()
-var msg = window.document.getElementById(`msg`)
-var img = window.document.getElementById(`foto`)
-
-var data= new Date()
-var hora = data.getHours()
-msg.innerText = `Agora são ${hora} horas:`
