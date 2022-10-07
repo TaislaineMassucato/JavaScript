316e6b48d0a138a68b6294469ab89d161a8b6f15
TaislaineMassucato <104451227+TaislaineMassucato@users.noreply.github.com>
2022-10-07 10:55:26 -0300

    operacoes

---
+++ Operacoes/operacao.js
@@ -0,0 +1,4 @@
+let x = 2
+x+=8
+
+console.log(`${x}`)
--- TipoVariaveis/exer.js
+++ TipoVariaveis/exer.js
@@ -1,9 +1,11 @@
 //Crie variavel para aarmazenar o nome do carro e coloca valor ferrari. e exiba.
 let carro = "Ferrari";
 console.log(`${carro}`);
+
 //crie variavel para armazenar o preço do bolo, que custa r$ 19,99.e exiba.
-let PrecoBolo = parseFloat(19.99)
-console.log(`Preço do bo és ${PrecoBolo}`)
+let PrecoBolo = parseFloat(19.99);
+console.log(`Preço do bo és ${PrecoBolo}`);
+
 //corrija codigo abaixo (let "cidade = sao pailo")
-let cidade = "Sao paulo"
-console.log(`Cidade ${cidade}`)
+let cidade = "Sao paulo";
+console.log(`Cidade ${cidade}`);
