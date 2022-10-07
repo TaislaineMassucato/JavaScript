39946bf2a947fc8b43c4ff05b35028b4820abb88
TaislaineMassucato <104451227+TaislaineMassucato@users.noreply.github.com>
2022-10-07 11:07:24 -0300

    operacao exer

--- Operacoes/operacao.js
+++ Operacoes/operacao.js
@@ -1,4 +1,15 @@
-let x = 2
-x+=8
+let n1= 22;
+let n2= 25;
+let n3= 24;
+let n4= 26;
+ 
+let res = (n1/n2)+(n3*n4);
+console.log(`${res}`);
 
-console.log(`${x}`)
+//operacao com contatenação string
+let nome ="Taislaine";
+let sobrenome ="Massucato";
+
+let NomeCompleto = nome+sobrenome;
+
+console.log(`${nome} ${sobrenome}`)
