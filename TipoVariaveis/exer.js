2cc6e44d62542c3eec62f50bdd1691bbf97f359b
TaislaineMassucato <104451227+TaislaineMassucato@users.noreply.github.com>
2022-10-07 10:42:22 -0300

    create exer js

---
+++ TipoVariaveis/exer.js
@@ -0,0 +1,9 @@
+//Crie variavel para aarmazenar o nome do carro e coloca valor ferrari. e exiba.
+let carro = "Ferrari";
+console.log(`${carro}`);
+//crie variavel para armazenar o preço do bolo, que custa r$ 19,99.e exiba.
+let PrecoBolo = parseFloat(19.99)
+console.log(`Preço do bo és ${PrecoBolo}`)
+//corrija codigo abaixo (let "cidade = sao pailo")
+let cidade = "Sao paulo"
+console.log(`Cidade ${cidade}`)
