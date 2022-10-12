64350872d53cc37d07e4816f9db0f677cbc6a69e
TaislaineMassucato <104451227+TaislaineMassucato@users.noreply.github.com>
2022-10-12 15:48:03 -0300

    colecao de html em spread array

--- operadorspread/spread.js
+++ operadorspread/spread.js
@@ -1,5 +1,21 @@
-let jogador1= {nome:"jao", magia:10, força:56 , vida:100};
-let jogador2= {nome:"jao", magia:10, força:56 , vida:100 , velocidade:23 , dor:32};
-let jogador3= {...jogador1,...jogador2};
+// let jogador1= {nome:"jao", magia:10, força:56 , vida:100};
+// let jogador2= {nome:"jao", magia:10, força:56 , vida:100 , velocidade:23 , dor:32};
+// let jogador3= {...jogador1,...jogador2};
 
-console.log (jogador3);
+// console.log (jogador3)
+
+// const soma=(n1,n2,n3)=>{
+//     return n1+n2+n3
+// }
+
+const obj1 = document.getElementsByTagName("div")
+const obj2 = [...document.getElementsByTagName("div")]
+
+console.log(obj1)
+console.log(obj2)
+
+// let valores=[1,2,3]
+
+// console.log(soma(...valores))
+
+//console.log(soma(valores)) *Nao vai porke os numeros estao juntos*
