71534125b093bcfe5f8d394293bde02ebf95efe1
TaislaineMassucato <104451227+TaislaineMassucato@users.noreply.github.com>
2022-10-14 11:45:02 -0300

    renember

--- loops/breakcontinue.js
+++ loops/breakcontinue.js
@@ -23,3 +23,13 @@ for(i; i < max; i++){
 console.log(`número de pares ${par}`)
 
 
+///Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido. 
+
+
+
+do{
+    var nota = parseInt(prompt("Digite uma nota de 0 a 10: "))
+    if(nota < 0 || nota > 10){
+        alert("Notas 0 a 10 APENASSSS!!!")
+    }
+}while(nota >=0 && nota <=10)
