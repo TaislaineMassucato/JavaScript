421886aad9e5c78e4fbb09f8da22446b5b32b7cd
TaislaineMassucato <104451227+TaislaineMassucato@users.noreply.github.com>
2022-10-14 13:18:46 -0300

    exer funcion

---
+++ funcao/funcio.js
@@ -0,0 +1,9 @@
+function mudartexto(){
+    let t1 = document.getElementById('d1')
+    let t2 = document.getElementById('d2')
+    let t3 = document.getElementById('d3')
+
+    t1.innerHTML="Oi Mudei"
+    t2.innerHTML="Ainda taislaine"
+    t3.innerHTML="Eterna studient tecnologia"
+}
